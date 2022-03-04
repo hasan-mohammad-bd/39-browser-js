0e7cf794c1c649a012c3f9c57a4ca759ea99748d
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-04 10:26:32 +0600

    adding some info

---
+++ 39-2-alert/39-2-alert.js
@@ -0,0 +1,36 @@
+console.log("I am here");
+
+// alert("he is coming")
+
+//alert
+//===========================================
+const maComing = () => {
+    alert("he is coming, open the book")
+}
+
+//confirm
+//get true and false 
+//===========================================
+const  askPicnic = () => {
+    const response = confirm("are you going to picnic");
+    console.log(response);
+    if(response === true){
+        alert("amake fee ta bKash kore de")
+    }
+    else {
+        console.log("dore gea mor");
+    }
+}
+
+//prompt 
+//to get input
+//===========================================
+const askName = () => {
+    const name = prompt("what is your name")
+    if (name) {
+        console.log(name);
+    }
+
+}
+
+//the will use "model" instead using "alert".
