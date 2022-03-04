9d899a60a93435d38a9f78486dba096d4a6114bc
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-04 09:39:28 +0600

    adding some info

---
+++ 39-1-explore.js
@@ -0,0 +1,13 @@
+const name = 15;
+const student = {name: "jolii", friend: "Borsha", job:"business",
+makeMovie: function(name){
+    console.log(name);
+}}
+student.makeMovie("din raat");
+
+//the same way below is a object which can be found in browser.
+//this is also called browser api, or BOM(Browser Object Model)
+document.getElementById("name");
+//document is related to browser, can't be found in local console.
+
+//find article about "introduction to web APIs" 
